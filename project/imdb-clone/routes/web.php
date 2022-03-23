a80669c295ce05b87d3a0fd1352cdbfa7ab8ae75
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-03-23 22:50:42 +0100

    Revert to remove breeze auth

--- project/imdb-clone/routes/web.php
+++ project/imdb-clone/routes/web.php
@@ -7,12 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-
-
-
-require __DIR__ . '/auth.php';
