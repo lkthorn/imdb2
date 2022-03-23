c466ac8ef37af943b10c3666b2b09f452c0ba69c
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-03-23 22:57:35 +0100

    delete everything

---
+++ project/imdb-clone/storage/framework/views/a97fcb86d9b7d5ad5c01905031ad548d4486475a.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>IMDB</title>
+</head>
+<body>
+    
+
+<article>
+    <h1>First Movie</h1>
+
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        At repellat praesentium ut? Accusantium eum ipsam nostrum, id commodi, corporis ad natus enim pariatur modi nam corrupti error magnam, esse omnis!
+    </p>
+
+</article>
+
+
+
+
+
+</body>
+</html><?php /**PATH /var/www/html/imdb-clone/resources/views/index.blade.php ENDPATH**/ ?>
---
+++ project/imdb-clone/storage/framework/views/c1377e6744a176396498da5b8453db3c4df2ece4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies</title>
+</head>
+<body>
+    <h1>Movies page</h1>
+
+    <p><?php echo e($title); ?></p>
+    <p><?php echo e($description); ?></p>
+
+</body>
+</html><?php /**PATH /var/www/html/imdb-clone/resources/views/movies/index.blade.php ENDPATH**/ ?>
