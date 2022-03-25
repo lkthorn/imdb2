e162383283ad5a2b553ec25e22ca87b99b4297ca
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-03-26 00:28:15 +0100

    Add styling of the landing page and navbar

--- project/imdb-clone/resources/views/index.blade.php
+++ project/imdb-clone/resources/views/index.blade.php
@@ -1,6 +1,249 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>Index of movies</div>
+   <section class="container mx-auto px-4 pt-16">
+    <!-- popular movies section -->
+    <div class="popular-movies">
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+
+        <!-- Grid for all movies -->
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-6 gap-8">
+            <!-- The image of the movie -->
+            <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>  
+            
+              
+
+        </div>
+    </div>
+    <!-- now playing section -->
+    <div class="now-playing-movies py-24">
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Now playing</h2>
+
+        <!-- Grid for all movies -->
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-6 gap-8">
+            <!-- The image of the movie -->
+            <div class="mt-8"> 
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div> 
+               <div class="mt-8">
+                <a href="#">
+                    <img src="/img/movie1.jpeg" alt="pulp fiction" class="hover:opacity-90">
+                </a>
+                <!-- Information about the movie -->
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">Pulp Fiction</a>
+                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>Feb 20, 1997</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>  
+            
+              
+
+        </div>
+    </div>
+
+
+   </section>
 @endsection
 
--- project/imdb-clone/resources/views/layouts/main.blade.php
+++ project/imdb-clone/resources/views/layouts/main.blade.php
@@ -7,48 +7,55 @@
     <title>IMDB CLONE</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
-<body class="font-sans bg-blue-600 text-white">
-    <nav class="border-b border-gray-800 bg-blue-900">
-        <section class="container mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
+
+
+<body class="font-sans text-white bg-gray-900">
+
+    <!-- Start of navbar -->
+    <nav class="border-b border-gray-900 bg-gray-800">
+        <section class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
+
+            <ul class="flex flex-col md:flex-row items-center">
                 <li>
-                    <a href="#">
-                        IMDB
-                    </a>
+                    <a class="logo" href="#">IMDB CLONE</a>
                 </li>
-                <li class="ml-16">
+
+                <li class="md:ml-16 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Movies</a>
                 </li>
-                <li class="ml-6">
+
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Tv Shows</a>
                 </li>
-                <li class="ml-6">
+
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Actors</a>
                 </li>
-                <li class="ml-6">
+
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Genres</a>
                 </li>
-                <li class="ml-6">
+
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Watchlist</a>
                 </li>
             </ul>
 
-            <ul class="flex items-center">
-                <div class="relative">
-                    <input type="text" class="bg-gray-800 rounded-full w-100 pl-8 px-10 py-2" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-gray-500 mt-3 ml-2" viewBox="0 0 20 20">
-							<path d="M18.175,4.142H1.951C1.703,4.142,1.5,4.344,1.5,4.592v10.816c0,0.247,0.203,0.45,0.451,0.45h16.224c0.247,0,0.45-0.203,0.45-0.45V4.592C18.625,4.344,18.422,4.142,18.175,4.142 M4.655,14.957H2.401v-1.803h2.253V14.957zM4.655,12.254H2.401v-1.803h2.253V12.254z M4.655,9.549H2.401V7.747h2.253V9.549z M4.655,6.846H2.401V5.043h2.253V6.846zM14.569,14.957H5.556V5.043h9.013V14.957z M17.724,14.957h-2.253v-1.803h2.253V14.957z M17.724,12.254h-2.253v-1.803h2.253V12.254zM17.724,9.549h-2.253V7.747h2.253V9.549z M17.724,6.846h-2.253V5.043h2.253V6.846z"></path>
-						</svg>
-                    </div>
+            <!-- Search bar -->
+            <ul class="flex flex-col md:flex-row mt-3 md:mt-0 items-center">
+                <div class="relative mt-3 md:mt-0">
+                    <input type="text" class="text-gray-900 bg-white-800 text-sm rounded-full w-100 pl-5 px-10 py-2" placeholder="Search">
                 </div>
             </ul>
+
+
+
         </section>
-
-
-
-
     </nav>
     @yield('content')
+
+    
+
+
 </body>
 </html>
